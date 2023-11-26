0c41f0b76529483a7b9ec4a4ce8b9d0075d0dc90
Fizzbow <55476631+Fizzbow@users.noreply.github.com>
2023-11-26 17:26:57 +0800

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  content: ["index.html", "./src/**/*.{js,jsx,ts,tsx,vue,html}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {},
+      fontFamily: { Poppins: ["Poppins", "sans-serif"] },
+    },
   },
   plugins: [],
-}
+};
