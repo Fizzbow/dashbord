1d587d69fe2dcd3668d28cd03189cf7f20d5c863
Fizzbow <55476631+Fizzbow@users.noreply.github.com>
2023-11-27 11:56:36 +0800

    style:nav style

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Nav from "./components/Nav";
 import logo from "./logo.svg";
 import poweredBy from "./powered-by-vitawind-dark.png";
 
@@ -6,8 +7,13 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="deep-linear min-h-screen ">
-      <h1 className="font-Poppins"></h1>
+    <div className="deep-linear flex min-h-screen flex-row items-center justify-center px-10">
+      <div
+        className="dashboard-content content-fill flex
+       max-w-4xl flex-1  rounded-md p-6"
+      >
+        <Nav />
+      </div>
     </div>
   );
 }
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,53 @@
-import React from "react";
-import PropTypes from "prop-types";
+// import React from "react";
+// import PropTypes from "prop-types";
+import "./nav.scss";
+import { useState } from "react";
 
-const Nav = (props) => {
-  return <div>Nav</div>;
+const Nav = (props: any) => {
+  return (
+    <header className="flex flex-row items-center">
+      <div className="h-12 w-12 bg-[url('src/assets/heartlogo.svg')] bg-contain bg-center" />
+      <div className="text-2xl font-bold text-white">DO PE</div>
+      <Select />
+
+      <div className="nav-link flex flex-row gap-2">
+        <span className="font-extralight text-[#878EB9]">Vkontakte</span>
+        <span className="font-extralight text-[#878EB9]">Tumblr</span>
+        <span className="font-extralight text-[#878EB9]">Odnoklassniki</span>
+      </div>
+    </header>
+  );
 };
 
 Nav.propTypes = {};
+
+const Select = (props) => {
+  const [flag, setFlag] = useState("progress");
+  return (
+    <div className="nav-select mx-4 flex items-center justify-center p-3">
+      <div className="nav-select_flag">flagg</div>
+
+      <div className="nav-select_icon mx-4">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="15"
+          height="10"
+          viewBox="0 0 15 10"
+          fill="none"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M14.3763 4.02132L9.11318 8.73207C8.32066 9.44141 7.04043 9.44141 6.2479 8.73207L0.984739 4.02132C-0.295491 2.87546 0.618959 0.91113 2.42754 0.911131L12.9539 0.911132C14.7624 0.911133 15.6566 2.87546 14.3763 4.02132Z"
+            fill="#878EB9"
+          />
+        </svg>
+      </div>
+    </div>
+  );
+};
+Select.propTypes = {
+  flag: "pansexual" | "progress" | "trans",
+};
 
 export default Nav;
