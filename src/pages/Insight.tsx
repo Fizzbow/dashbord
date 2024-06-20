26001b2dc0b7d8916bd6313d150c03c1810a2413
Fizzbow <55476631+Fizzbow@users.noreply.github.com>
2024-06-20 19:51:32 +0800

    feat:chart

--- src/pages/Insight.tsx
+++ src/pages/Insight.tsx
@@ -1,8 +1,19 @@
+import StockTrading from "@/components/Insight/StockTrading";
+import "./Insight.scss";
+
 const Insight = () => {
   return (
-    <div className="flex h-full bg-[#000] font-Poppins text-[#fff]">
-      Insight
-    </div>
+    <main className="insight flex h-full bg-[#000] font-Poppins justify-center items-center text-[#fff]">
+      <section className="h-[70%] w-[60%]">
+        <header className="py-2 rounded-lg bg-[#121212] flex flex-row px-3 items-center border border-[#2D2D2D] w-full">
+          <div className="flex flex-row item-center">
+            <span className="font-500 text-lg">Insight</span>
+            <div className="insight_logo w-6 h-6 relative" />
+          </div>
+        </header>
+        <StockTrading />
+      </section>
+    </main>
   );
 };
 
