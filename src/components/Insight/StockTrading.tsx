277afc8db269585ec4ae91181d1a3ce1accc616a
Fizzbow <55476631+Fizzbow@users.noreply.github.com>
2024-08-27 14:55:10 +0800

    feat(stock-trading):chart

--- src/components/Insight/StockTrading.tsx
+++ src/components/Insight/StockTrading.tsx
@@ -4,21 +4,54 @@ import {
   XAxis,
   YAxis,
   CartesianGrid,
-  Tooltip,
   Legend,
-  ResponsiveContainer,
   Area,
   ComposedChart,
 } from "recharts";
-import {
-  ChartConfig,
-  ChartContainer,
-  ChartTooltip,
-  ChartTooltipContent,
-} from "../ui/chart";
-import { Card, CardHeader } from "../ui/card";
-import { cn } from "@/lib/utils";
+import MotionNumber from "motion-number";
+import { ChartContainer, ChartTooltip, ChartTooltipContent } from "../ui/chart";
+import { CardHeader, CardTitle } from "../ui/card";
 import InsightCardContain from "./InsightCardContain";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+
+import { useState } from "react";
+import { motion } from "framer-motion";
+
+interface SelectProp {
+  val: number;
+  per: number;
+  bullish: number;
+}
+
+const selectData: Record<string, SelectProp> = {
+  apple: {
+    val: 5678,
+    per: 1.8,
+    bullish: 20,
+  },
+  banana: {
+    val: 788,
+    per: 2.8,
+    bullish: 40,
+  },
+  grapes: {
+    val: 1234,
+    per: 5.2,
+    bullish: -5,
+  },
+  pineapple: {
+    val: 5555,
+    per: 4.2,
+    bullish: 5,
+  },
+};
 
 const data = [
   {
@@ -70,17 +103,88 @@ const chartConfig = {
 };
 
 const StockTrading = () => {
+  const [selectKey, setSelectKey] = useState(() => {
+    const defaultKey = Object.keys(selectData)[0];
+    return defaultKey;
+  });
   return (
     <InsightCardContain className="col-start-1 col-end-5">
-      <CardHeader>AI Insight</CardHeader>
+      <CardHeader className="justify-between items-center">
+        <span>AI Insight</span>
+        <Select
+          onValueChange={(key) => {
+            if (key) {
+              setSelectKey(key);
+            }
+          }}
+        >
+          <SelectTrigger>
+            <SelectValue
+              defaultValue={selectKey}
+              placeholder="Select a fruit"
+            />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              {Object.keys(selectData).map((key) => (
+                <SelectItem value={key} key={key}>
+                  {key}
+                </SelectItem>
+              ))}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </CardHeader>
+      <CardTitle className="px-6 flex flex-row gap-2 pb-6">
+        {/* {JSON.stringify(selectData[selectKey].val)} */}
+        <MotionNumber
+          value={selectData[selectKey].val}
+          format={{ style: "currency", currency: "USD" }}
+          style={{ lineHeight: 0.85 }}
+          className="~text-xl/4xl font-semibold [--mask-height:0.25em]"
+          after={() => (
+            <MotionNumber
+              className="~text-base/2xl px-[0.3em] font-semibold [--mask-height:0.3em]"
+              value={selectData[selectKey].bullish}
+              format={{ style: "decimal" }}
+              style={{ borderRadius: 999, lineHeight: 0.85 }}
+              first={() => (
+                <motion.svg
+                  className="mr-0.5 size-[0.75em] self-center"
+                  viewBox="0 0 24 24"
+                  strokeWidth="3"
+                  vectorEffect="non-scaling-stroke"
+                  stroke="currentColor"
+                  transition={{
+                    rotate: { type: "spring", duration: 0.5, bounce: 0 },
+                  }}
+                  animate={{
+                    rotate: selectData[selectKey].bullish > 0 ? 0 : -180,
+                  }}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18"
+                  />
+                </motion.svg>
+              )}
+              animate={{
+                backgroundColor:
+                  selectData[selectKey].bullish > 0 ? "#34d399" : "#ef4444",
+              }}
+            />
+          )}
+        />
+      </CardTitle>
       <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
         <ComposedChart
           accessibilityLayer
           data={data}
           margin={{
             top: 5,
-            right: 30,
-            left: 20,
+            right: 20,
+            left: 0,
             bottom: 5,
           }}
         >
@@ -98,8 +202,8 @@ const StockTrading = () => {
             </defs>
           </defs>
           <CartesianGrid stroke="#383838" vertical={false} />
-          <XAxis dataKey="name" />
-          <YAxis axisLine={false} />
+          <XAxis tickLine={false} dataKey="name" />
+          <YAxis axisLine={false} tickLine={false} />
           <ChartTooltip content={<ChartTooltipContent />} />
           <Legend />
           <Line
